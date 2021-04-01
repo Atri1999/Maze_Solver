d2031b6731988dbe83d1d2abb3f7350011e0ab85
Atri Chakraborty <chakrabortyatri1999@gmail.com>
2021-04-01 11:51:56 +0530

    Solving real maze and added the time taken in main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import read_maze as rm
 import search 
 import graphing as g
+import time
 
+pic='Maze_4.png'
 
-pic='Maze3.png'
+start=time.perf_counter()
 maze=rm.Maze(pic)
 graph =g.Graph(maze.maze_to_array())
 
@@ -14,3 +16,7 @@ graph.detect_nodes()
 srh=search.Search(graph.start_node,graph.end_node,graph.graph)
 result=graph.decode_node(srh.dfs())
 maze.write_on_maze(result)
+
+end=time.perf_counter()
+
+print("The time required is :",(end-start))
